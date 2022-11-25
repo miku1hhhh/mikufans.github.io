2516d96f73dca1f57683d23674db0262840313d1
miku1hhhh <119019928+miku1hhhh@users.noreply.github.com>
2022-11-25 15:23:50 +0800

    Create feedback.php

---
+++ plus/feedback.php
@@ -0,0 +1,36 @@
+
+<center>
+<script>
+      var pgo=0;
+      function JumpUrl(){
+        if(pgo==0){ location='javascript:history.go(-1);'; pgo=1; }
+      }
+document.write("<br /><div style='width:450px;padding:0px;border:1px solid #D1DDAA;'><div style='padding:6px;font-size:12px;border-bottom:1px solid #D1DDAA;background:#DBEEBD url(/plus/img/wbg.gif)';'><b>Mikufans 提示信息！</b></div>");
+document.write("<div style='height:130px;font-size:10pt;background:#ffffff'><br />");
+document.write("文档id不能为空!");
+document.write("<br /><a href='javascript:history.go(-1);'>如果你的浏览器没反应，请点击这里...</a><br/></div>");
+setTimeout('JumpUrl()',5000);</script>
+</center>
+</body>
+</html>
+<!--
+     FILE ARCHIVED ON 06:40:19 Jul 17, 2009 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 07:17:32 Nov 25, 2022.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+-->
+<!--
+playback timings (ms):
+  captures_list: 221.216
+  exclusion.robots: 0.137
+  exclusion.robots.policy: 0.13
+  cdx.remote: 0.047
+  esindex: 0.006
+  LoadShardBlock: 194.288 (3)
+  PetaboxLoader3.datanode: 162.475 (4)
+  CDXLines.iter: 17.379 (3)
+  load_resource: 76.548
+  PetaboxLoader3.resolve: 29.052
+-->
