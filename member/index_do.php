e0662cd42d68b9dd66f6a0258eb89d61102d11c8
miku1hhhh <119019928+miku1hhhh@users.noreply.github.com>
2022-12-10 11:28:33 +0800

    Create index_do.php

---
+++ member/index_do.php
@@ -0,0 +1,51 @@
+<html>
+<head>
+</script>
+<
+<title>MiKufans提示信息</title>
+<meta http-equiv="Content-Type" content="text/html; charset=gb2312"/>
+<base target="_self"/>
+<style>div{line-height:160%;}</style></head>
+<body leftmargin="0" topmargin="0">
+	 
+	  <tr class="d">
+	    <td class="b" nowrap="nowrap"><img src="/_static/images/toolbar/wm_tb_prv_off.png" alt="Previous capture" width="14" height="16" border="0" /></td>
+	    <td class="c" id="displayDayEl" style="width:34px;font-size:22px;white-space:nowrap;" title="You are here: 10:22:45 Jul 19, 2009">19</td>
+	    <td class="f" nowrap="nowrap"><a href="miku1hhhh.Github.io/mikufans.Github.io/member/index_do.php" title="07:28:22 Nov 11, 2009"><img src="/_static/images/toolbar/wm_tb_nxt_on.png" alt="Next capture" width="14" height="16" border="0" /></a></td>
+	  </tr>
+
+<center>
+<script>
+      var pgo=0;
+      function JumpUrl(){
+        if(pgo==0){ location='index.php'; pgo=1; }
+      }
+document.write("<br /><div style='width:450px;padding:0px;border:1px solid #D1DDAA;'><div style='padding:6px;font-size:12px;border-bottom:1px solid #D1DDAA;background:#DBEEBD url(/plus/img/wbg.gif)';'><b>Mikufans 提示信息！</b></div>");
+document.write("<div style='height:130px;font-size:10pt;background:#ffffff'><br />");
+document.write("本页面禁止返回!");
+document.write("<br /><a href='index.php'>如果你的浏览器没反应，请点击这里...</a><br/></div>");
+setTimeout('JumpUrl()',1000);</script>
+</center>
+</body>
+</html>
+<!--
+     FILE ARCHIVED ON 10:22:45 Jul 19, 2009 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 03:16:24 Dec 10, 2022.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+-->
+<!--
+playback timings (ms):
+  captures_list: 233.665
+  exclusion.robots: 0.227
+  exclusion.robots.policy: 0.216
+  cdx.remote: 0.081
+  esindex: 0.01
+  LoadShardBlock: 79.115 (3)
+  PetaboxLoader3.datanode: 321.079 (4)
+  CDXLines.iter: 19.98 (3)
+  load_resource: 327.054
+  PetaboxLoader3.resolve: 47.268
+-->
